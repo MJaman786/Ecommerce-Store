e37e9f47aa9fc46299aeffdc7125d6526788c100
MJaman786 <amanmujawar064@gmail.com>
2025-08-26 11:32:06 +0530

    changed the data passing

---
+++ src/data/data.js
@@ -0,0 +1,6 @@
+export const myProducts = [
+    { id: 0, name: "SAMSUNG Galyxi", desc: "jasciua aiscuiac uasciuabsc uiu auisciu uasc bibasc uibausbc", img: "/img/phone.png", price: "30,000" },
+    { id: 0, name: "SAMSUNG Galyxi", desc: "jasciua aiscuiac uasciuabsc uiu auisciu uasc bibasc uibausbc", img: "/img/keypad.png", price: "30,000" },
+    { id: 0, name: "SAMSUNG Galyxi", desc: "jasciua aiscuiac uasciuabsc uiu auisciu uasc bibasc uibausbc", img: "/img/laptop.png", price: "30,000" },
+    { id: 0, name: "SAMSUNG Galyxi", desc: "jasciua aiscuiac uasciuabsc uiu auisciu uasc bibasc uibausbc", img: "/img/boat_headphones.png", price: "30,000" }
+];
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,14 +1,10 @@
 import "../styles/Products.css"
 import ProductsNo from "./ProductsNo";
+import { myProducts } from "../data/data";
 
 // import product1 from "../assets/img/phone.png"
 
-let myProducts = [
-    { id: 0, name: "SAMSUNG Galyxi", desc: "jasciua aiscuiac uasciuabsc uiu auisciu uasc bibasc uibausbc", img: "/img/phone.png", price: "30,000" },
-    { id: 0, name: "SAMSUNG Galyxi", desc: "jasciua aiscuiac uasciuabsc uiu auisciu uasc bibasc uibausbc", img: "/img/keypad.png", price: "30,000" },
-    { id: 0, name: "SAMSUNG Galyxi", desc: "jasciua aiscuiac uasciuabsc uiu auisciu uasc bibasc uibausbc", img: "/img/laptop.png", price: "30,000" },
-    { id: 0, name: "SAMSUNG Galyxi", desc: "jasciua aiscuiac uasciuabsc uiu auisciu uasc bibasc uibausbc", img: "/img/boat_headphones.png", price: "30,000" }
-];
+
 
 const Product = () => {
     return (
